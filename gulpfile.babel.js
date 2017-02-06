1534d6f308dcf236fcffde6b113c98f93b2c24b0
visdark <visdark@hotmail.com>
2017-02-06 18:36:40 +0800

    初始化环境

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,16 @@
  * gulp构架结构
  **/
 
+
+
 import gulp from 'gulp';
 import cleancss from 'gulp-clean-css';
 import debug from 'gulp-debug';
+import htmlmin from 'gulp-htmlmin';
+import template from 'gulp-template';
+
+
+//默认任务
 
 gulp.task('default', function() {
     // 将你的默认的任务代码放在这
@@ -12,4 +19,30 @@ gulp.task('default', function() {
     return gulp.src('')
         .pipe(debug({title: '开始执行第一个任务'}))
         .pipe(gulp.dest('dist'));
+});
+
+//浏览器测试
+var browserSync = require('browser-sync').create();
+gulp.task('browser', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/fxbtg"
+        }
+    });
+});
+
+// 压缩html
+gulp.task('html',function(){
+    gulp.src('./src/fxbtg-html/*.html')
+        .pipe(debug({title: '正在编译静态文件'}))
+        .pipe(template({pkg: require('./package.json')}))
+        .pipe(debug({title: '正在压缩静态文件'}))
+        .pipe(
+            htmlmin({collapseWhitespace: true ,
+                removeComments: true,
+                minifyJS:true
+            }))
+        .pipe(debug({title: '正在移动静态文件'}))
+        .pipe(gulp.dest('./dist/fxbtg/'))
+        .pipe(debug({title: '成功执行HTML压缩'}));
 });
